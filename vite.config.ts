1428d98cf572e53125d61433685e40c5812b24c3
daniel.anca <daniel.anca@eggs.ro>
2025-11-19 17:29:22 +0200

    update again

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 import type { UserConfig } from "vitest/config";
 
 const test = {
@@ -11,13 +11,37 @@ const test = {
   watch: false,
 } as UserConfig["test"];
 
-// https://vitejs.dev/config/
 const isProd = process.env.NODE_ENV === "production";
+const isHttps = process.env.USE_HTTPS === "true";
+const hmrProtocol = isHttps ? "wss" : "ws";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
+  esbuild: {
+    jsx: "automatic",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: { ".js": "jsx" },
+    },
+  },
+  server: {
+    // ascultă pe toate interfețele (nu doar localhost)
+    host: true, // sau "0.0.0.0"
+    port: 5858, // poți lăsa 3000 dacă vrei, dar să fie același peste tot
+    https: isHttps,
+    // 🔥 fix pt eroarea ta: "This host ('montanair.ro') is not allowed"
+    allowedHosts: ["montanair.ro", "www.montanair.ro"],
+    hmr: {
+      protocol: hmrProtocol,
+      host: "montanair.ro", // sau subdomeniul prin care intri
+      port: 24678,
+    },
+  },
   build: {
-    minify: false,
+    minify: isProd,
+    sourcemap: !isProd,
   },
   test,
 });
